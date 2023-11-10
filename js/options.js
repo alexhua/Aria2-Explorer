1953e010af1588c56f5427ee55dd06c14724f45b
Alex Hua <sealexh@gmail.com>
2023-11-11 03:03:23 +0800

    chore: Add tip for system level magnet capture

--- js/options.js
+++ js/options.js
@@ -34,6 +34,11 @@ var Configs =
             $("#keepAwake").prop("disabled", !$("#monitorAria2").prop("checked"));
         })
 
+        if (Utils.getPlatform() == "Windows") {
+            let tooltip = chrome.i18n.getMessage("captureMagnetTip")
+            $("#captureMagnet").parent().addClass("tool-tip tool-tip-icon");
+        }
+
         for (const input of $("input[type=text],input[type=number]")) {
             if (Configs[input.id])
                 input.value = Configs[input.id];
